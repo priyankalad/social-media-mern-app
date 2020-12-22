eeedf297b791a64c11056b3d2e4889497c003915
Priyanka Lad <priyanka.i.lad@gmail.com>
2020-12-22 17:07:05 +0530

    change NODE_ENV to production

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./main.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
